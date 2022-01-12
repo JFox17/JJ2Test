92ac0e09365d0dccf11bccb4613ce5f325266539
Julia V <rif1707@mail.ru>
2022-01-12 22:27:47 +0300

    Промежуточный вариант

---
+++ components/Tickets.vue
@@ -0,0 +1,116 @@
+<template>
+<div class="tickets">
+  <div class="tickets__container">
+    <h3 class="tickets__title">цены на вход и пользование<br> всеми аттракционами</h3>
+  </div>
+  <div class="tariff">
+      <div class="tariff__weekdays">
+        <p class="tariff__text">Будни</p>
+        <div class="tariff__info">
+          <div class="tariff__block">
+            <img :src="require(`@/assets/img/small-infinity.png`)" />
+            <div>
+              <p class="tariff__price">Входной билет <br><span class="tariff__price-dop">(на целый день)</span></p>
+              <p class="tariff__price-large">950 руб.</p>
+            </div>
+          </div>
+          <div class="tariff__block">
+            <img :src="require(`@/assets/img/hour.png`)" />
+            <div>
+              <p class="tariff__price">Входной билет <br><span class="tariff__price-dop">(на целый день)</span></p>
+              <p class="tariff__price-large">950 руб.</p>
+            </div>
+          </div>
+          <div class="tariff__block">
+            <img :src="require(`@/assets/img/next.png`)" />
+            <div>
+              <p class="tariff__price">Входной билет <br><span class="tariff__price-dop">(на целый день)</span></p>
+              <p class="tariff__price-large">950 руб.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tariff__weekend">
+        <p class="tariff__text">выходные <br><span class="tariff__text-dop">и праздничные дни</span></p>
+        <div class="tariff__info">
+          <img :src="require(`@/assets/img/infinity.png`)" />
+          <p class="tariff__price">Входной билет <br><span class="tariff__price-dop">(на целый день)</span></p>
+          <p class="tariff__price-large">950 руб.</p>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+<style scoped lang="scss">
+.tickets {
+  width: 100%;
+  &__title{
+    margin-top: 84px;
+    margin-bottom: 62px;
+    font-family: 'Roboto-Black';
+    font-size: 30px;
+    font-weight: 400px;
+    color: #758185;
+    text-transform: uppercase;
+    letter-spacing: 6px;
+  }
+}
+.tariff {
+  background: #fafbfc;
+  display: flex;
+  justify-content: center;
+  justify-content: space-evenly;
+  &__text {
+    font-family: 'Roboto-Bold';
+    font-size: 36px;
+    line-height: 25px;
+    font-weight: 700;
+    color: #758185;
+    text-transform: uppercase;
+  }
+  &__text-dop {
+    font-size: 16px;
+    line-height: 20px;
+  }
+  &__info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #758185;
+    margin-top: 38px;
+  }
+  &__info > img {
+    width: 105px;
+    height: 50px;
+    margin-bottom: 32px;
+  }
+  &__price {
+    font-family: 'Roboto-Regular';
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 18px;
+  }
+  &__price-dop {
+    font-size: 16px;
+  }
+  &__price-large {
+    font-family: 'Roboto-Bold';
+    font-size: 36px;
+    font-weight: 700;
+  }
+  &__block {
+    display: flex;
+    align-items: center;
+    &:first-child {
+      margin-bottom: 34px;
+    }
+    &:nth-child(2) {
+      margin-bottom: 34px;
+    }
+  }
+  &__block > img {
+    height: 100%;
+    margin-right: 16px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,74 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        JJ2Test
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <h2 class="title">
+      цены joki joya в санкт-петербурге
+    </h2>
+    <div class="wrap">
+      <button
+      v-for="(tab, idx) in tabs"
+      :key="idx"
+      :class="['tab-button', { active: currentTab === tab.comp }]"
+      @click="currentTab = tab.comp"
+    >
+      <img :src="require(`@/assets/img/${tab.img}`)" /> {{ tab.title }}
+    </button>
     </div>
+    <keep-alive>
+      <component v-bind:is="currentTab" class="tab"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-export default {}
+import Tickets from '../components/Tickets.vue'
+import Rooms from '../components/Rooms.vue'
+import Table from '../components/Table.vue'
+import Kitchen from '../components/Kitchen.vue'
+import Decorations from '../components/Decorations.vue'
+export default {
+  data() {
+    return {
+      currentTab: "Tickets",
+      tabs: [
+        {
+          title: "Билеты",
+          comp: "Tickets",
+          img: "tickets.png"
+        },
+        {
+          title: "Комнаты",
+          comp: "Rooms",
+          img: 'rooms.png'
+        },
+        {
+          title: "Столы",
+          comp: "Table",
+          img: 'dinner-table.png'
+        },
+        {
+          title: "Рестораны",
+          comp: "Kitchen",
+          img: 'kitchen.png'
+        },
+        {
+          title: "Праздники",
+          comp: "Decorations",
+          img: 'decorations.png'
+        },
+      ]
+    }
+  },
+  components: {
+    Tickets,
+    Rooms,
+    Table,
+    Kitchen,
+    Decorations
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -39,35 +76,51 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  font-family: 'Marvin-Round';
+  background: linear-gradient(-2deg, #cca06a 0%, #d3ab78 30%, #b17c3b 70%, #e4c6a8 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: table;
+  color: #cda472;
+  font-size: 52px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: normal;
+  line-height: 51.75px;
+  text-align: center;
+  margin-bottom: 54px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.tab-button {
+  font-family: 'Roboto-Regular';
+  background: #e1e8eb;
+  color: #758185;
+  font-weight: 400;
+  // line-height: 108.01px;
+  font-size: 24px;
+  letter-spacing: -0.6px;
+  // padding: 16px 25px;
+  height: 91px;
+  max-width: 223px;
+  width: 100%;
+  margin-right: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  // height: 100%;
 }
+.active {
+  background: #758185;
+  color: #ffff;
+}
+.wrap {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1130px;
 
-.links {
-  padding-top: 15px;
 }
 </style>
